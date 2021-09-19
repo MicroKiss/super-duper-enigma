60296918086b9ba630303ded6a41a1f93cdff0f1
Kiss János <32140381+MicroKiss@users.noreply.github.com>
2021-09-19 10:37:03 +0200

    Delete egyetemi dolgok/public_html/pages/a directory

--- egyetemi dolgok/public_html/pages/a/plain html_js_css/global.js
+++
@@ -1,6 +0,0 @@
-var global = {
-    query: document.querySelector("#query"),
-    top_bar: document.querySelector("#top-bar"),
-    content: document.querySelector("#content"),
-    mockdata: [],
-};
